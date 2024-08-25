839bd6a3ed8d84b3210c9e5e2843ed1140e96487
Mrityuanjaya Gupta <mrityuanjaya.gupta@joshtechnologygroup.com>
2024-08-25 20:46:28 +0530

    Added Meals Page and loading page

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,13 +1,38 @@
+import { Suspense } from "react";
 import Link from "next/link";
+
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
+import MealsLoadingPage from "./loading-out";
+
+const Meals = async () => {
+    const meals = await getMeals();
+    return <MealsGrid meals={meals} />;
+};
 
 const MealsPage = () => {
     return (
-        <main style={{ textAlign: "center" }}>
-            <h1>Meals Page</h1>
-            <Link style={{ color: "white" }} href="/meals/share">
-                Go to meals share page
-            </Link>
-        </main>
+        <>
+            <header className={classes.header}>
+                <h1>
+                    Delicious Meals, created{" "}
+                    <span className={classes.highlight}>by you</span>
+                </h1>
+                <p>
+                    Choose your favorite recipe and cook it yourself. It is easy
+                    and fun!
+                </p>
+                <p className={classes.cta}>
+                    <Link href="meals/share">Share your favorite recipe.</Link>
+                </p>
+            </header>
+            <main className={classes.main}>
+                <Suspense fallback={<MealsLoadingPage />}>
+                    <Meals />
+                </Suspense>
+            </main>
+        </>
     );
 };
 
